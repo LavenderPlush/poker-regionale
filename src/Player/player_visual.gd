4fec3dab5111331162aa989eef737e502903ae92
juules32 <56916699+Juules32@users.noreply.github.com>
2025-10-04 19:10:48 +0200

    added moving camera

--- src/Player/player_visual.gd
+++ src/Player/player_visual.gd
@@ -6,10 +6,24 @@ const CARD: PackedScene = preload("uid://dq5coxtle62he")
 
 var positions: Array[Node3D] = []
 var hand: Array = []
+const FIXED_X_DEG: float = deg_to_rad(-18.0)
 
 func _ready() -> void:
 	for c in position_holder.get_children():
 		positions.append(c)
+	print(rotation)
+
+@onready var rotation_goal = rotation
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var screen_center = get_viewport().get_visible_rect().size / 2
+		var diff = screen_center - event.position
+		rotation_goal.y = diff.x / 10000
+		rotation_goal.x = diff.y / 10000 + FIXED_X_DEG
+
+func _physics_process(delta: float) -> void:
+	rotation = rotation.move_toward(rotation_goal, delta * (rotation - rotation_goal).length() * 10)
 
 func draw_cards(cards: Array[Card]):
 	clear_hand()
