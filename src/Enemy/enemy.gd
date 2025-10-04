d255bec03705558341f6ced140a3022fe17d25d7
Olivier <oliha@itu.dk>
2025-10-04 17:32:23 +0200

    Add visuals to enemies and hands

---
+++ src/Enemy/enemy.gd
@@ -0,0 +1,40 @@
+extends Sprite3D
+
+const npc1_front_sprite: Texture2D = preload("res://Assets/NPC1_Front.png")
+const npc2_side_sprite: Texture2D = preload("res://Assets/NPC2_Side.png")
+
+@onready var card_holder: Node3D = $CardHolder
+
+var hand: Array
+
+const npc_textures = [
+	npc1_front_sprite,
+	npc2_side_sprite
+]
+
+enum NPC {
+	NPC1_front,
+	NPC2_side
+}
+
+@export var flip_sprite: bool = false
+@export var npc_texture: NPC = NPC.NPC1_front
+
+func _ready() -> void:
+	texture = npc_textures[npc_texture]
+	hand = card_holder.get_children()
+	show_cardbacks()
+
+func hide_cards():
+	card_holder.visible = false
+
+func show_cards():
+	card_holder.visible = true
+	
+func display_cards(cards: Array[Card]):
+	for i in range(hand.size()):
+		hand[i].display(cards[i])
+
+func show_cardbacks():
+	for card in hand:
+		card.display(1,4)
